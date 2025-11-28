c191d38e3633d9024c4af654b8a8f713338b3745
NguyenKhietMinh <minhkhietnguyen1910@gmail.com>
2025-11-28 15:10:02 +0700

    Module Search & Booking: create TC Upload Image- fix

--- src/main/java/pages/UserProfileForm.java
+++ src/main/java/pages/UserProfileForm.java
@@ -158,22 +158,6 @@ public class UserProfileForm extends BasePages {
         System.out.println("Mo form upload hinh anh");
     }
 
-    public static String getRandomJpg(String directoryPath) {
-        File folder = new File(directoryPath);
-
-        File[] jpgFiles = folder.listFiles((dir, name) ->
-                name.toLowerCase().endsWith(".jpg")
-        );
-
-        if (jpgFiles == null || jpgFiles.length == 0) {
-            throw new RuntimeException("Không tìm thấy file JPG trong thư mục: " + directoryPath);
-        }
-
-        Random random = new Random();
-        File randomFile = jpgFiles[random.nextInt(jpgFiles.length)];
-
-        return randomFile.getAbsolutePath();
-    }
 
 
 }
